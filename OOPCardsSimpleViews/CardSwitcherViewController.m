68aaebc6901f7d1cd59611e1ad84cc53fc6d45bb
ganchau <gancchau@gmail.com>
2015-06-10 17:17:16 -0400

    lab completed

--- OOPCardsSimpleViews/CardSwitcherViewController.m
+++ OOPCardsSimpleViews/CardSwitcherViewController.m
@@ -9,6 +9,9 @@
 #import "CardSwitcherViewController.h"
 
 @interface CardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
 
 @end
 
@@ -20,5 +23,10 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)buttonPressed:(UIButton *)sender {
+    self.topLabel.text = sender.titleLabel.text;
+    self.middleLabel.text = sender.titleLabel.text;
+    self.bottomLabel.text = sender.titleLabel.text;
+}
 
 @end
